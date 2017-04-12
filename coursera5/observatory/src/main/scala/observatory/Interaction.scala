3e142c902a6612173b42714d58a5c094cb8629e5
ryandavidhartman <ryandavidhartman@gmail.com>
2017-04-11 21:11:00 -0400

    working on Course #5 homework #3

--- coursera5/observatory/src/main/scala/observatory/Interaction.scala
+++ coursera5/observatory/src/main/scala/observatory/Interaction.scala
@@ -1,6 +1,9 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
+import observatory.Visualization.{interpolateColor, pixelPositionToLocation, predictTemperature}
+
+import scala.math._
 
 /**
   * 3rd milestone: interactive visualization
@@ -14,7 +17,9 @@ object Interaction {
     * @return The latitude and longitude of the top-left corner of the tile, as per http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
     */
   def tileLocation(zoom: Int, x: Int, y: Int): Location = {
-    ???
+    val lat = toDegrees(atan(sinh(Pi * (1.0 - 2.0 * y.toDouble / (1<<zoom)))))
+    val lon = x.toDouble / (1<<zoom) * 360.0 - 180.0
+    Location(lat, lon)
   }
 
   /**
@@ -26,8 +31,20 @@ object Interaction {
     * @return A 256×256 image showing the contents of the tile defined by `x`, `y` and `zooms`
     */
   def tile(temperatures: Iterable[(Location, Double)], colors: Iterable[(Double, Color)], zoom: Int, x: Int, y: Int): Image = {
-    ???
+    val pixelArray = scala.collection.mutable.ArrayBuffer[Pixel]()
+
+    for(y_iter <- (0 until 256)) {
+      for(x_iter <- (0 until 256)) {
+        val location = tileLocation(zoom, (x+x_iter), (y-y_iter))
+        val color = interpolateColor(colors, predictTemperature(temperatures, location))
+        val pixel = Pixel(color.red, color.green, color.blue, 127);
+        pixelArray += pixel
+      }
+    }
+
+    Image(360,180, pixelArray.toArray)
   }
+
 
   /**
     * Generates all the tiles for zoom levels 0 to 3 (included), for all the given years.
