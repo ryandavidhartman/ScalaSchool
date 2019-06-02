ea5dc30fae8a75dfca16547fe2f636f979c15d9c
ryandavidhartman <ryandavidhartman@gmail.com>
2019-06-01 20:11:32 -0400

    better week1 answers

--- coursera1/week1/recfun/src/main/scala/recfun/Main.scala
+++ coursera1/week1/recfun/src/main/scala/recfun/Main.scala
@@ -13,51 +13,43 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
+    * Exercise 1
+    */
   def pascal(c: Int, r: Int): Int = {
-    if(c <= 0 || c >= r)
-      1
-    else
-      pascal(c-1,r-1) + pascal(c,r-1)
+    assert(r >= 0)
+    assert(c <= r)
+
+    if (c == 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
-  
+
   /**
-   * Exercise 2
-   */
+    * Exercise 2
+    */
   def balance(chars: List[Char]): Boolean = {
     @tailrec
-    def helper(acc:Int, chars: List[Char]): Boolean = {
-      if(chars.isEmpty) {
-        acc == 0
-      }
-      else {
-        val next = acc + (chars.head match {
-          case '(' => 1
-          case ')' => -1
-          case _ => 0
-        })
-
-        if(next < 0) false else helper(next, chars.tail)
-      }
+    def helper(acc: Int, chars: List[Char]): Boolean = chars match {
+      case Nil => acc == 0
+      case c :: cs => if (c == '(') helper(acc + 1, cs)
+      else if (acc <= 0) false
+      else helper(acc - 1, cs)
     }
 
-    helper(0,chars)
+    helper(0, chars.filter(c => c == '(' || c == ')'))
   }
 
-    /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = {
-      val sortedCoins = coins.sorted
 
-      def count(money:Int, index:Int):Int = {
-        if (money == 0) 1
-        else if(money < 0 || index < 0) 0
-        else count(money, index-1) + count(money-sortedCoins(index), index)
-      }
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    val sortedCoins = coins.sortWith((l, r) => l >= r)
 
-      count(money, coins.length-1)
+    def helper(money: Int, index: Int): Int = {
+      if (money == 0) 1
+      else if (money < 0 || index >= sortedCoins.length) 0
+      else helper(money, index + 1) + helper(money - sortedCoins(index), index)
     }
 
+    helper(money, 0)
+  }
 }
