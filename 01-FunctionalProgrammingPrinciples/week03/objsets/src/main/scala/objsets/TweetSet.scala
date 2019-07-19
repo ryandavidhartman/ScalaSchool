cfc02b5250a085ed7ac02e0ca4bbdf26fd271feb
ryandavidhartman <ryandavidhartman@gmail.com>
2019-07-19 11:14:01 -0400

    updating week3 homework

--- 01-FunctionalProgrammingPrinciples/week03/objsets/src/main/scala/objsets/TweetSet.scala
+++ 01-FunctionalProgrammingPrinciples/week03/objsets/src/main/scala/objsets/TweetSet.scala
@@ -41,8 +41,8 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def filter(p: Tweet => Boolean): TweetSet = ???
-  
+    def filter(p: Tweet => Boolean): TweetSet = filterAcc(p, new Empty)
+
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
    */
@@ -54,8 +54,8 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def union(that: TweetSet): TweetSet = ???
-  
+    def union(that: TweetSet): TweetSet
+
   /**
    * Returns the tweet from this set which has the greatest retweet count.
    *
@@ -65,8 +65,8 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def mostRetweeted: Tweet = ???
-  
+    def mostRetweeted: Tweet
+
   /**
    * Returns a list containing all tweets of this set, sorted by retweet count
    * in descending order. In other words, the head of the resulting list should
@@ -76,8 +76,8 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def descendingByRetweet: TweetList = ???
-  
+    def descendingByRetweet: TweetList
+
   /**
    * The following methods are already implemented
    */
@@ -104,11 +104,13 @@ abstract class TweetSet {
    * This method takes a function and applies it to every element in the set.
    */
   def foreach(f: Tweet => Unit): Unit
+
+  def isEmpty: Boolean
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
+
   /**
    * The following methods are already implemented
    */
@@ -120,13 +122,24 @@ class Empty extends TweetSet {
   def remove(tweet: Tweet): TweetSet = this
 
   def foreach(f: Tweet => Unit): Unit = ()
+
+  def union(that: TweetSet): TweetSet = that
+
+  def isEmpty: Boolean = true
+
+  def mostRetweeted:Tweet = throw new java.util.NoSuchElementException
+
+  def descendingByRetweet: TweetList = Nil
 }
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
-    
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = {
+      val newAcc = if(p(this.elem)) acc.incl(this.elem) else acc
+      left.filterAcc(p, right.filterAcc(p, newAcc))
+    }
+
+
   /**
    * The following methods are already implemented
    */
@@ -151,6 +164,25 @@ class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
     f(elem)
     left.foreach(f)
     right.foreach(f)
+  }
+
+  def isEmpty = false
+
+  def union(that: TweetSet): TweetSet = this.left.union(this.right.union(that.incl(this.elem)))
+
+  def mostRetweeted: Tweet = {
+    lazy val leftMostRetweeted = left.mostRetweeted
+    lazy val righMostRetweeted = right.mostRetweeted
+
+    val leftAndCenter = if(!left.isEmpty && leftMostRetweeted.retweets > elem.retweets) leftMostRetweeted else elem
+    val rightAndCenter = if(!right.isEmpty && righMostRetweeted.retweets > elem.retweets) righMostRetweeted else elem
+
+    if(leftAndCenter.retweets > rightAndCenter.retweets) leftAndCenter else rightAndCenter
+  }
+
+  def descendingByRetweet: TweetList = {
+    val most = this.mostRetweeted
+    new Cons(most, this.remove(most).descendingByRetweet)
   }
 }
 
@@ -182,7 +214,7 @@ object GoogleVsApple {
 
     lazy val googleTweets: TweetSet = ???
   lazy val appleTweets: TweetSet = ???
-  
+
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
