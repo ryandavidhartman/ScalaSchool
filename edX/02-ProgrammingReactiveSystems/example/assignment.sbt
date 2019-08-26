0d72806f04e2e84d4b49e456da74ff869d7e64af
ryandavidhartman <ryandavidhartman@gmail.com>
2019-08-25 20:06:53 -0400

    adding the example homework assignment

---
+++ edX/02-ProgrammingReactiveSystems/example/assignment.sbt
@@ -0,0 +1,12 @@
+enablePlugins(StudentPlugin)
+
+course := "progfun1"
+assignment := "example"
+
+assignmentInfo := ch.epfl.lamp.AssignmentInfo(
+  key = "g4unnjZBEeWj7SIAC5PFxA",
+  itemId = "xIz9O",
+  premiumItemId = None,
+  partId = "d5jxI",
+  styleSheet = Some((_: File) / "scalastyle" / "scalastyle_config.xml")
+)
---
+++ edX/02-ProgrammingReactiveSystems/example/src/main/scala/example/Lists.scala
@@ -0,0 +1,57 @@
+package example
+
+
+object Lists {
+
+  /**
+   * This method computes the sum of all elements in the list xs. There are
+   * multiple techniques that can be used for implementing this method, and
+   * you will learn during the class.
+   *
+   * For this example assignment you can use the following methods in class
+   * `List`:
+   *
+   *  - `xs.isEmpty: Boolean` returns `true` if the list `xs` is empty
+   *  - `xs.head: Int` returns the head element of the list `xs`. If the list
+   *    is empty an exception is thrown
+   *  - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
+   *    list `xs` without its `head` element
+   *
+   *  ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
+   *  solution.
+   *
+   * @param xs A list of natural numbers
+   * @return The sum of all elements in `xs`
+   */
+  // def sum(xs: List[Int]): Int = xs.fold(0)((l, r) => l+r) or
+  // def sum(xs: List[Int]): Int = xs.fold(0)(_ + _)
+  // But this is probably what they want
+  def sum(xs: List[Int]): Int = if(xs.isEmpty) 0 else xs.head + sum(xs.tail)
+
+
+  /**
+   * This method returns the largest element in a list of integers. If the
+   * list `xs` is empty it throws a `java.util.NoSuchElementException`.
+   *
+   * You can use the same methods of the class `List` as mentioned above.
+   *
+   * ''Hint:'' Again, think of a recursive solution instead of using looping
+   * constructs. You might need to define an auxiliary method.
+   *
+   * @param xs A list of natural numbers
+   * @return The largest element in `xs`
+   * @throws java.util.NoSuchElementException if `xs` is an empty list
+   */
+    def max(xs: List[Int]): Int = {
+      if(xs.isEmpty) throw new NoSuchElementException
+
+      def helper(max:Int, ys: List[Int]): Int = {
+        if(ys.isEmpty) max
+        else {
+          val newMax = if(max >= ys.head) max else ys.head
+          helper(newMax, ys.tail)
+        }
+      }
+      helper(xs.head, xs.tail)
+    }
+  }
