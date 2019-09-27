563dde84c05b144ba41d91429a83b27755f44d6a
ryandavidhartman <ryandavidhartman@gmail.com>
2019-09-27 12:03:09 -0400

    week 2 link checker update

---
+++ edX/02-ProgrammingReactiveSystems/week02/reactive-link-checker/src/main/scala/reactive/BadCache.scala
@@ -0,0 +1,69 @@
+package reactive
+
+import java.util.concurrent.Executor
+
+import akka.actor.{Actor, ActorRef}
+import reactive.BadCache2.Result
+import reactive.Receptionist.Get
+import akka.pattern.pipe
+
+import scala.concurrent.ExecutionContext
+
+/*
+When futures and actors are mixed you can also have synchronization issues.
+Since future composition methods tempt you to close over the actor's state
+ */
+
+class BadCache extends Actor {
+  var cache = Map.empty[String, String]
+
+  implicit val executor: Executor with ExecutionContext = context.dispatcher.asInstanceOf[Executor with ExecutionContext]
+
+
+  override def receive: Receive = {
+    case Get(url) =>
+      if(cache contains url )
+        sender ! cache(url)
+      else
+        WebClient get url foreach { body =>
+          // This happens in the scope of the callback on the future
+          // not thread safe with the actor!
+          cache += url -> body
+          sender ! body
+        }
+  }
+
+}
+
+object BadCache2 {
+  case class Result(sender: ActorRef, url: String, body: String)
+}
+
+class BadCache2 extends Actor {
+  var cache = Map.empty[String, String]
+
+  implicit val executor: Executor with ExecutionContext = context.dispatcher.asInstanceOf[Executor with ExecutionContext]
+
+
+  override def receive: Receive = {
+    case Get(url) =>
+      if(cache contains url )
+        sender ! cache(url)
+      else
+        WebClient get url map { r => Result(sender, url, r) } pipeTo self
+    case Result(client, url, body) =>
+      cache += url -> body
+      sender ! body
+  }
+
+  /*But this actor contains another problem.
+The transformation described by the map operation on the future,
+runs the code which you give it, in the future.
+And that means that, the sender will be accessed.
+In the future.
+This is problematic, because sender is giving
+you the actor which corresponds to the actor, which has sent the message, which is currently
+being processed, but when that future runs
+the actor might do something completely different
+
+}
--- edX/02-ProgrammingReactiveSystems/week02/reactive-link-checker/src/main/scala/reactive/Controller.scala
+++ edX/02-ProgrammingReactiveSystems/week02/reactive-link-checker/src/main/scala/reactive/Controller.scala
@@ -17,12 +17,47 @@ object Controller {
 class Controller extends Actor with ActorLogging {
   import Controller._
 
+  // In the lecture Roland makes a point that it is much
+  // better to use a variable pointing to an immutable set
+  // than a val pointing to a mutable set for the cache
+  //
+
+  /*
+  For example, this controller might be used
+to process another query, keeping the cache.
+Would be a valid use case, but then the result which had been sent back
+to the parent, points to the same set, which is mutated by this actor.
+And then the other actor will be confused, as to
+what the contents of the set are. It is much better to prefer using
+variables here, which point to immutable data structures.
+This way they can safely be shared as is done here.
+   */
+
   var cache = Set.empty[String]
   var children = Set.empty[ActorRef]
 
+  // Start a 10 second timer after *any* message is processed
   context.setReceiveTimeout(10.seconds)
 
-  def receive = {
+
+
+  /* Alternatively
+
+  import context.dispatcher
+  context.system.scheduler.scheduleOnce(10.seconds, self, ReceiveTimeout)
+
+  //Do not do this:
+  import context.dispatcher
+  context.system.scheduler.scheduleOnce(10.seconds) {
+    context.children foreach (_ ! Getter.Abort)
+  }
+
+  The above it NOT thread safe!  It is executed context of the scheduler not
+  this actor.  Therefore the children is used on another thread
+*/
+
+
+  def receive: PartialFunction[Any, Unit] = {
     case Check(url, depth) =>
       log.debug("{} checking {}", depth, url)
       if (!cache(url) && depth > 0)
