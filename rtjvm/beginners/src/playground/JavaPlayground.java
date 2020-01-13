9d20aa133b6125e275e90210cf3499a196235ac6
ryandavidhartman <ryandavidhartman@gmail.com>
2020-01-12 20:38:12 -0500

    getting ready to rock the jvm, beginner style

---
+++ rtjvm/beginners/src/playground/JavaPlayground.java
@@ -0,0 +1,9 @@
+package playground;
+
+public class JavaPlayground {
+
+    public static void main(String args[]) {
+        System.out.println("Hello Java!");
+
+    }
+}
---
+++ rtjvm/beginners/src/playground/ScalaPlayground.scala
@@ -0,0 +1,5 @@
+package playground
+
+object ScalaPlayground extends App {
+  println("Hello Scala!")
+}
