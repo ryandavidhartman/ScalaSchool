c0a6f106e90bd78d4a9a7160672c1d166878ce78
Ryan Hartman <ryandavidhartman@gmail.com>
2020-09-02 21:06:20 -0400

    finishing 2.56 in scheme

--- sicp/src/main/scala/chapter02/section03/subsection02/ex02_56.rkt
+++ sicp/src/main/scala/chapter02/section03/subsection02/ex02_56.rkt
@@ -5,9 +5,31 @@
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
 
-(define (make-sum a1 a2) (list '+ a1 a2))
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
 
-(define (make-product m1 m2) (list '* m1 m2))
+;(define (make-sum a1 a2) (list '+ a1 a2))
+
+;(define (make-product m1 m2) (list '* m1 m2))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (make-exponentiation b e)
+  (cond ((=number? e 0) 1)
+        ((=number? e 1) b)
+        ((=number? b 0) 0)
+        (else (list '** b e))))
 
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
@@ -15,11 +37,18 @@
 (define (product? x)
   (and (pair? x) (eq? (car x) '*)))
 
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
 (define (addend s) (if (sum? s) (cadr s) (error "not a sum")))
 (define (augend s) (if (sum? s) (caddr s) (error "not a sum")))
 
 (define (multiplier p) (if (product? p)(cadr p) (error "not a product")))
 (define (multiplicand p) (if (product? p) (caddr p) (error "not a product")))
+
+(define (base e) (if (exponentiation? e)(cadr e) (error "not exponentiation")))
+(define (exponent e) (if (exponentiation? e)(caddr e) (error "not an exponentiation")))
+
 
 (define (deriv exp var)
   (cond ((number? exp) 0)
@@ -34,8 +63,21 @@
                          (deriv (multiplicand exp) var))
            (make-product (deriv (multiplier exp) var)
                          (multiplicand exp))))
+         ((exponentiation? exp)
+          (make-product (exponent exp)
+                        (make-product (make-exponentiation (base exp) (- (exponent exp) 1))
+                                      (deriv (base exp) var))))
         (else
          (error "unknown expression type -- DERIV" exp))))
 
 ; x+3
 (deriv '(+ x 3) 'x)
+
+; xy
+(deriv '(* x y) 'x)
+
+; xy(x+3)
+(deriv '(* (* x y) (+ x 3)) 'x)
+
+;;  x^6
+(deriv '(** (* y x) 6) 'x)
--- sicp/src/main/scala/chapter02/section03/subsection02/ex02_56.sc
+++ sicp/src/main/scala/chapter02/section03/subsection02/ex02_56.sc
@@ -1,5 +1,6 @@
 import ScalaScheme.Primitives.{SD, SL, caddr, cadr, car, isPair, schemeDataToSchemeList}
 import ScalaScheme.SchemeList
+import ScalaScheme.SchemeMath.{sum,multiply}
 
 import scala.util.{Failure, Success, Try}
 
@@ -8,9 +9,32 @@ def isVariable(x: SD): Boolean = x.isInstanceOf[Symbol]
 def isSameVariable(v1: SD,  v2: SD): Boolean =
   isVariable(v1) && isVariable(v2) && v1.equals(v2)
 
-def makeSum(a1:SD, a2:SD): SL = SchemeList(Symbol("+"), a1, a2)
+def isNumber(x: SD): Boolean =
+  Try {
+    x.toString.toDouble
+  } match {
+    case Success(_) => true
+    case Failure(_) => false
+  }
 
-def makeProduct(m1: SD,m2: SD): SL = SchemeList(Symbol("*"), m1, m2)
+def isEqualNum(x:SD, y:SD): Boolean = isNumber(x) && x == y
+
+//def makeSum(a1:SD, a2:SD): SL = SchemeList(Symbol("+"), a1, a2)
+
+//def makeProduct(m1: SD,m2: SD): SL = SchemeList(Symbol("*"), m1, m2)
+
+def makeSum(a1:SD, a2:SD): SD =
+  if(isEqualNum(a1, 0)) a2
+  else if(isEqualNum(a2, 0)) a1
+  else if(isNumber(a1) && isNumber(a2)) sum(a1,a2)
+  else SchemeList(Symbol("+"), a1, a2)
+
+def makeProduct(m1: SD,m2: SD): SD =
+  if(isEqualNum(m1, 0) || isEqualNum(m2, 0)) 0
+  else if(isEqualNum(m1, 1)) m2
+  else if(isEqualNum(m2, 1)) m1
+  else if(isNumber(m1) && isNumber(m2)) multiply(m1,m2)
+  else SchemeList(Symbol("*"), m1, m2)
 
 def isSum(x: SD): Boolean = isPair(x) && (car(x) == Symbol("+"))
 
@@ -23,14 +47,6 @@ def augend(s: SD): SD = if(isSum(s)) caddr(s) else throw new IllegalArgumentExce
 def multiplier(p: SD): SD = if(isProduct(p)) cadr(p) else throw new IllegalArgumentException("not a product")
 
 def multiplcand(p: SD): SD = if(isProduct(p)) caddr(p) else throw new IllegalArgumentException("not a product")
-
-def isNumber(x: SD): Boolean =
-  Try {
-    x.toString.toDouble
-  } match {
-    case Success(_) => true
-    case Failure(_) => false
-  }
 
 def deriv(exp: SD, variable:Symbol): SD =
   if(isNumber(exp))
