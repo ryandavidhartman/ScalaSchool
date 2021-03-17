6e13f324eaefb8c4f28fb66ca888f21392df7598
ryandavidhartman <ryandavidhartman@gmail.com>
2021-03-16 21:14:38 -0400

    more sugary syntax

--- rtjvm/advanced/src/main/scala/section2/05-Dark-Sugars.scala
+++ rtjvm/advanced/src/main/scala/section2/05-Dark-Sugars.scala
@@ -1,9 +1,10 @@
 package section2
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
+import scala.util.control.NonFatal
 
 object DarkSugars extends App {
-  // #1 Methods with single parameters
+  //  Syntax Sugar One: Methods with single parameters
 
   def singleArgMethod(arg: Int): String= arg.toString  // random method that takes a single argument
   // you can leave off the (), since this can be implied
@@ -29,10 +30,60 @@ object DarkSugars extends App {
     throw new RuntimeException
   }
 
+  lazy val block = {
+    // code
+    throw new Exception("dfd")
+  }
 
+  val anotherTryInstance: Try[Int] = Try.apply(block)
 
+  val finalTryInstance: Try[Int] = try Success(block) catch {
+    case NonFatal(e) => Failure(e)
+  }
 
+  // example b)  map, flatMap, and filter => these are single argument functions
+  List(1,2,3) map { x =>
+    x+1
+  }
 
+  List(1,2,3).map(x => x+1)
+
+  // Syntax Sugar Two: single abstract method
+
+  trait Action {
+    def act(x: Int): Int
+  }
+
+  // don't care what this does, but the second argument is an instance of the Action Trait
+  def doAction(thingToActOn: Int,  action: Action): Int = action.act(thingToActOn)
+
+  doAction(10, (r:Int) => r + 1)  // this works!  That lambda can "stand in" for an Action
+  // Action is a trait with an single abstract method.  Of type Int => Int.  So anything that is of type
+  // Int => Int will work as an action
+
+  val action1: Action = new Action {
+    override def act(x: Int): Int = x + 1
+  }
+
+  val action2: Action = (x: Int) => x + 1  //Lambda to Single Abstract Method conversion
+
+  // example a:  Runnables
+  val thread1: Thread = new Thread(new Runnable {
+    override def run(): Unit = println("Very fancy runnable")
+  })
+
+  val thread2: Thread = new Thread(() => println("Still fancy"))
+
+  // this also works with Traits/Asstract Classes that have implemented methods:
+
+  trait WithSomeImplementations {
+    def add_1(x: Int): Int = x + 1
+    def add_2(x: Int): Int = x + 2
+    def sammy(s: String): String
+  }
+
+  val withSome: WithSomeImplementations = (s: String) => s.toUpperCase
+  withSome.add_1(1)
 
 
 
