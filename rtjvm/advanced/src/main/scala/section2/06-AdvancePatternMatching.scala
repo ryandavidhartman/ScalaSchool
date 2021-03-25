e28c8baef1d1c157a1801876af9e015e94edbb6d
ryandavidhartman <ryandavidhartman@gmail.com>
2021-03-24 20:56:30 -0400

    more pattern matching

--- rtjvm/advanced/src/main/scala/section2/06-AdvancePatternMatching.scala
+++ rtjvm/advanced/src/main/scala/section2/06-AdvancePatternMatching.scala
@@ -1,5 +1,8 @@
 package section2
 
+import scala.util.Try
+import scala.util.control.NonFatal
+
 object AdvancedPatternMatching extends App {
 
   val numbers = List(1)
@@ -23,16 +26,41 @@ object AdvancedPatternMatching extends App {
   // First make a companion object for Person
 
   object Person {
-    // Now we can destructure instances of the Person class!
+    // Now we can de-structure instances of the Person class!
     def unapply(person: Person): Option[(String, Int)] = Some(person.name, person.age)
   }
 
   val bob = new Person(name = "Bob", age = 34)
 
-  val (name: String, age: Int) = bob match {
-    case Person(n, a) => (n, a)
+  val greeting = bob match {
+    case Person(n, a) => s"Hi $n you are $a years old!"
+  }
+  println(greeting)
+
+  object LegalPerson {
+    def unapply(p: Person): Option[(String, Int)] = if(p.age > 21) Some((p.name, p.age)) else None
+
+    def unapply(age: Int): Option[String] = Some(if(age > 21) "Legal" else "Minor")
   }
 
+  val legalPersonGreeting = bob match {
+    case LegalPerson(n, a) => s"We have a legal person named $n who is $a years old"
+  }
+
+  println(legalPersonGreeting)
+
+
+  Try {
+    new Person("Youngster", 8) match {
+      case LegalPerson(n, a) => s"We have a legal person named $n who is $a years old"
+    }
+  }.recover {
+    case NonFatal(e) => println(e)
+  }
+
+  val status = 19 match {
+    case LegalPerson(s: String) => s
+  }
 
 
 }
