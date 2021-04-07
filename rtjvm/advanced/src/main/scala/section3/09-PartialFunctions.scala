48811bfbee558a3c9edb66e12654ac8b4984c6cc
ryandavidhartman <ryandavidhartman@gmail.com>
2021-04-07 19:57:14 -0400

    Lecture 9

--- rtjvm/advanced/src/main/scala/section3/09-PartialFunctions.scala
+++ rtjvm/advanced/src/main/scala/section3/09-PartialFunctions.scala
@@ -4,7 +4,7 @@ import scala.util.{Failure, Try}
 
 object PartialFunctions extends App {
 
-  val aFunction = (x: Int) => x + 1  // Function1[Int,Int] or the sugared type Int => Int
+  val aNormalFunction = (x: Int) => x + 1  // Function1[Int,Int] or the sugared type Int => Int
 
   // In mathematics this function is defined for all x elements of the set of Integers.
   // This is a "regular" function.
@@ -18,6 +18,7 @@ object PartialFunctions extends App {
     else
       throw new FunctionNotApplicableException
 
+
   // Here fussyFunction1 is only applicable for |x| <= 10.  It fails for all other values
   // You can also write this with a pattern match
 
@@ -30,18 +31,19 @@ object PartialFunctions extends App {
     case x:Int if x >= -10 && x <= 10 => x*x
   }
 
-  println(aPartialFunction(5))
+  assert(aPartialFunction(5) == 25)
 
-  Try {
+  val goForIt = Try {
     aPartialFunction(11)
   } match {
-    case Failure(e) => println(e)
+    case Failure(e) => println(s" Calling a PF out of range gives a:  $e")
   }
 
   // Note fussyFunction1 and fussyFunction2 are proper functions, that just happen to
   // below up for certain inputs. The are of type Function1[Int, Int]
 
   // aPartialFunction is of type PartialFunction[Int, Int]
+  // PartialFunction is a subtype of Function1
 
   //////////////////////////////////////////////////////////
   // SOME PARTIAL FUNCTION UTILITIES
@@ -50,10 +52,59 @@ object PartialFunctions extends App {
   // isDefinedAt will test to see if the partial function is defined for that part of
   // the domain.
 
-  println(aPartialFunction.isDefinedAt(1))
-  println(aPartialFunction.isDefinedAt(12))
+  assert(aPartialFunction.isDefinedAt(1) == true)
+  assert(aPartialFunction.isDefinedAt(12) == false)
 
+  // You can "lift" a partial function to a regular function that returns an option:
 
+  val liftedFunction = aPartialFunction.lift
+
+  assert(liftedFunction(12) == None)
+  assert(liftedFunction(5) == Option(25))
+
+  // Partial Functions can be "chained" with orElse
+  val superChain = aPartialFunction.orElse[Int, Int] {
+    case 101 => 202
+  }
+  assert(superChain(101) == 202)
+
+  // Since Partial Functions extend normal Functions we are do this:
+
+  val aTotalFunction: Int => Int = aPartialFunction
+
+  val aSecondFunction: Int => Int = {
+    case 1 => 99
+  }
+
+  // This means anything that accepts a HOF will also accept a partial function!
+  val aMappedList  = List(1, 2, 3) map {
+    case 1 => 42
+    case 2 => 43
+    case 3 => 44
+  }
+  assert(aMappedList == List(42, 43, 44))
+
+  // quick side note: this blows up:
+
+  Try {
+    List(1, 2, 3, 4) map {
+      case 1 => 42
+      case 2 => 43
+      case 3 => 44
+    }
+  } match {
+    case Failure(e) => println(e)
+  }
+
+  // Use COLLECT, collect is just like map but it expects a Partial Function
+  // and just ignores values outside the PF's domain!
+
+  val collected = List(1, 2, 3, 4) collect {
+    case 1 => 42
+    case 2 => 43
+    case 3 => 44
+  }
+  assert(collected == aMappedList )
 
 
 
