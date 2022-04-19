d94455e243e41b4729a2a95090c19bd8eab89f75
ryandavidhartman <ryandavidhartman@gmail.com>
2022-04-18 21:20:07 -0400

    adding contains

--- datastructures/src/main/scala/example/trees/MyTree.scala
+++ datastructures/src/main/scala/example/trees/MyTree.scala
@@ -5,13 +5,21 @@ import scala.annotation.tailrec
 // Simple BST
 
 sealed trait MyTree[+T] {
-  def insert[U >: T](data: U)(implicit ordering: Ordering[U]): MyTree[U]
+  def insert[U >: T](datae: U)(implicit ordering: Ordering[U]): MyTree[U]
+
+  def isEmpty(): Boolean
+  def contains[U >: T](element: U)(implicit ordering: Ordering[U]): Boolean
+
   def foreach(f: T => Unit): Unit
 }
 
 case object Leaf extends MyTree[Nothing] {
   override def insert[U >: Nothing](data: U)(implicit ordering: Ordering[U]): MyTree[U] =
     Node(data, Leaf, Leaf)
+
+  override def isEmpty(): Boolean = true
+  override def contains[U >: Nothing](element: U)(implicit ordering: Ordering[U]): Boolean = false
+
   override def foreach(f: Nothing => Unit): Unit = ()
 
 }
@@ -25,6 +33,16 @@ case class Node[+T](node: T, left: MyTree[T], right: MyTree[T]) extends MyTree[T
       Node(node, left, right.insert(data))
     else
       Node(node, left.insert(data), right)
+  }
+
+  override def isEmpty(): Boolean = false
+  override def contains[U >: T](element: U)(implicit ordering: Ordering[U]): Boolean = {
+    if(element == node)
+      true
+    else if(ordering.gt(node, element))
+      left.contains(element)
+    else
+      right.contains(element)
   }
 
   override def foreach(f: T => Unit): Unit = {
--- datastructures/src/test/scala/trees/MyTreeSpec.scala
+++ datastructures/src/test/scala/trees/MyTreeSpec.scala
@@ -5,10 +5,20 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class MyTreeSpec  extends AnyFlatSpec with Matchers {
-  "MyTree trait" should "construct new Trees" in {
+  "MyTree" should "construct new Trees" in {
     val testTree = MyTree(1,2,3)
     val expectedTree = Node(1, Leaf, Node(2, Leaf, Node(3, Leaf, Leaf)))
 
     assert(testTree == expectedTree)
+  }
+
+  "MyTree" should "be able to find elements" in {
+    val testTree1 = Node(1, Leaf, Leaf)
+    assert(testTree1.contains(1))
+    assert(!testTree1.contains(3))
+
+    val testTree2 = MyTree(1,2,3)
+    assert(testTree2.contains(2))
+    assert(!testTree2.contains(4))
   }
 }
